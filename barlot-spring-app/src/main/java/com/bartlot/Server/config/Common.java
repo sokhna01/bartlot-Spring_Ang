a4fb67836d6c06aab5161524594c42866c3291b8
PaM <matar0236@gmail.com>
2023-06-27 12:35:34 +0000

    commit

--- barlot-spring-app/src/main/java/com/bartlot/Server/config/Common.java
+++ barlot-spring-app/src/main/java/com/bartlot/Server/config/Common.java
@@ -2,7 +2,8 @@ package com.bartlot.Server.config;
 
 public class Common {
     public static int maxDayCompteur = 34;
-    public static String meterDataPath = "/Users/maoumi/Desktop/ANTG/fichiers_excel/";
+
+    public static String meterDataPath = "/Users/papa matar/Desktop/";
     public static String filename = "donnees_compteur.xlsx";
     public static double SeuilRed1 = 0.4;
     public static double SeuilRed2 = 0.7;
